2872c7bd0f0b70c43c2eb2724e0b2500aed9d1a6
Suzanne Goncalves <suzanne.g.p@hotmail.fr>
2023-02-28 16:17:06 +0100

    add photo test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+require "open-uri"
 
 Adventure.destroy_all
 User.destroy_all
 
 user1 = User.create!(email:"toto@gmail.com", password:"123456", username:"toto premier")
 
+file_jungle1 = URI.open("https://res.cloudinary.com/dtlwkdqpv/image/upload/v1677593896/Koh-Lanta/Jungle/jungle1_uoggke.jpg")
   jungle = Adventure.create(title: 'Jungle Book', location: 'Pandora', decription: 'dans le monde extraterrestre luxuriant de Pandora vivez comme  les Navi, des êtres qui semblent primitifs, mais qui sont très évolués', price: 34000 )
+  jungle.photo.attach(io: file_jungle1, filename: "jungle1_uoggke.jpg", content_type: "image/jpg")
+  jungle.save
   island = Adventure.create(title: 'Fantastic Island', location: 'Thailande', decription: 'Vivez une expérience unique et découvrez une autre vision du voyage, durable et engagée.', price: 34000 )
   desert = Adventure.create(title: 'Desert Experience', location: 'Merzouga', decription: 'Faire l expérience d un trek dans le désert, c est vivre des moments uniques et privilégiés au cœur de la nature et de la culture ..', price: 34000)
   mountain = Adventure.create(title: 'Mountain Exploration', location: 'Tyrol', decription: 'Se la jouer Mike Horn en stage de survie ', price: 34000)
