efed1cc96a18554f66ea6aa89e9cfd1f6d38a0bd
hadrien mari <hadrienmari@yahoo.fr>
2023-03-01 10:48:30 +0100

    homepage + seeds2

--- app/views/show.html.rb
+++
@@ -1,5 +0,0 @@
-<h2><%= @adventure.title %></h2>
-<p><%= @adventure.decription %></p>
-<p><%= @adventure.location %></p>
-<p><%= @adventure.price %></p>
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,22 @@ User.destroy_all
 user1 = User.create(email:"toto@gmail.com", password:"123456", username:"toto premier")
 
 file_jungle1 = URI.open("https://res.cloudinary.com/dtlwkdqpv/image/upload/v1677593896/Koh-Lanta/Jungle/jungle1_uoggke.jpg")
-  jungle = Adventure.create(user: user1, title: 'Jungle Book', location: 'Pandora', decription: 'dans le monde extraterrestre luxuriant de Pandora vivez comme  les Navi, des êtres qui semblent primitifs, mais qui sont très évolués', price: 34000 )
+  jungle = Adventure.create!(user: user1, title: 'Jungle Book', location: 'Pandora', decription: 'dans le monde extraterrestre luxuriant de Pandora vivez comme  les Navi, des êtres qui semblent primitifs, mais qui sont très évolués', price: 34000 )
   jungle.photos.attach({io: file_jungle1, filename: "jungle1_uoggke.jpg", content_type: "image/jpg"})
   jungle.save!
-  island = Adventure.create(title: 'Fantastic Island', location: 'Thailande', decription: 'Vivez une expérience unique et découvrez une autre vision du voyage, durable et engagée.', price: 34000 )
-  desert = Adventure.create(title: 'Desert Experience', location: 'Merzouga', decription: 'Faire l expérience d un trek dans le désert, c est vivre des moments uniques et privilégiés au cœur de la nature et de la culture ..', price: 34000)
-  mountain = Adventure.create(title: 'Mountain Exploration', location: 'Tyrol', decription: 'Se la jouer Mike Horn en stage de survie ', price: 34000)
+  file_jungle1 = URI.open("https://res.cloudinary.com/dtlwkdqpv/image/upload/v1677593896/Koh-Lanta/Jungle/jungle1_uoggke.jpg")
+  island = Adventure.create!(user: user1, title: 'Fantastic Island', location: 'Thailande', decription: 'Vivez une expérience unique et découvrez une autre vision du voyage, durable et engagée.', price: 34000 )
+ island.photos.attach({io: file_jungle1, filename: "jungle2_uoggke.jpg", content_type: "image/jpg"})
+ island.save!
+ file_jungle1 = URI.open("https://res.cloudinary.com/dtlwkdqpv/image/upload/v1677593896/Koh-Lanta/Jungle/jungle1_uoggke.jpg")
+  desert = Adventure.create!(user: user1, title: 'Desert Experience', location: 'Merzouga', decription: 'Faire l expérience d un trek dans le désert, c est vivre des moments uniques et privilégiés au cœur de la nature et de la culture ..', price: 34000)
+ desert.photos.attach({io: file_jungle1, filename: "jungle3_uoggke.jpg", content_type: "image/jpg"})
+ desert.save!
+ file_jungle1 = URI.open("https://res.cloudinary.com/dtlwkdqpv/image/upload/v1677593896/Koh-Lanta/Jungle/jungle1_uoggke.jpg")
+  mountain = Adventure.create!(user: user1, title: 'Mountain Exploration', location: 'Tyrol', decription: 'Se la jouer Mike Horn en stage de survie ', price: 34000)
+ mountain.photos.attach({io: file_jungle1, filename: "jungle4_uoggke.jpg", content_type: "image/jpg"})
+  mountain.save!
+  file_jungle1 = URI.open("https://res.cloudinary.com/dtlwkdqpv/image/upload/v1677593896/Koh-Lanta/Jungle/jungle1_uoggke.jpg")
+  arctic = Adventure.create!(user: user1, title: 'arctic Exploration', location: 'arctic island', decription: 'gla gla ', price: 34000)
+  arctic.photos.attach({io: file_jungle1, filename: "jungle5_uoggke.jpg", content_type: "image/jpg"})
+  arctic.save!
