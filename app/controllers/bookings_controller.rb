c823323904e432e16e06fef2a89fda25ba81e63c
hadrien mari <hadrienmari@yahoo.fr>
2023-03-02 17:37:31 +0100

    booking renvois sur dashbord

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,21 @@
 class BookingsController < ApplicationController
-
   def new
     @booking = Booking.new
+    @adventure = Adventure.find(params[:adventure_id])
+    @user = current_user
   end
 
   def create
+    @user = current_user
     @adventure = Adventure.find(params[:adventure_id])
     @booking = Booking.new(booking_params)
     @booking.adventure = @adventure
     @booking.user = current_user
-    @booking.save
+    if @booking.save
+      redirect_to profil_path, status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
@@ -17,6 +23,4 @@ class BookingsController < ApplicationController
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
-
-
 end
